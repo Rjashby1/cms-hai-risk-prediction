cc2efbcaca620dc8bfb6683d707c2f39cb31dd0e
Alysa Pugire <alysapugmire@gmail.com>
2026-03-13 20:29:30 -0400

    Start work on the baseline logistic regression

---
+++ code/06_baseline_log_reg.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, RandomizedSearchCV,StratifiedKFold, TunedThresholdClassifierCV
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeine import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, f1_score, precision_recall_curve
+
+def run_baseline_logreg(data, target, features):
+    X = data[features]
+    y = data[target]
+
+    # Split the data into training and testing sets
+    Xtrain, Xtest, ytrain,ytest = train_test_split(X, y, test_size=0.2, random_state=5, stratify=y)
+
+    # Specify cross-validation strategy
+    cv = StratifiedKFold(n_splits=5, random_state=5, shuffle=True)
+
+    # Build preprecessing for pipeline
+
+
